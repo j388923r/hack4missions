037c68e33352281ab35e709d31e34b24d0117743
Jamar Brooks <j388923r@gmail.com>
2015-12-29 15:52:53 -0600

    added files to create views

---
+++ public/javascripts/articlesController.js
@@ -0,0 +1,5 @@
+var myApp = angular.module('myApp');
+
+myApp.controller('articlesController', ['$scope', function($scope) {
+  
+}]);
--- public/javascripts/myApp.js
+++ public/javascripts/myApp.js
@@ -16,5 +16,26 @@ myApp.config(['$routeProvider', function($routeProvider) {
   }).when('/connect', {
 	  controller : 'connectController',
 	  templateUrl : '/angularViews/connect.html'
+  }).when('/category', {
+	  controller : 'categoryController',
+	  templateUrl : '/angularViews/category.html'
+  }).when('/articles', {
+	  controller : 'articlesController',
+	  templateUrl : '/angularviews/articles.html'
+  }).when('/articles/:id', {
+	  controller : 'articleController',
+	  templateUrl : '/angularviews/article.html'
+  }).when('/videos', {
+	  controller : 'videosController',
+	  templateUrl : '/angularviews/videos.html'
+  }).when('/videos/:id', {
+	  controller : 'videoController',
+	  templateUrl : '/angularviews/video.html'
+  }).when('/podcasts', {
+	  controller : 'podcastsController',
+	  templateUrl : '/angularviews/podcasts.html'
+  }).when('/podcasts/:id', {
+	  controller : 'podcastController',
+	  templateUrl : '/angularviews/podcast.html'
   });
 }]);

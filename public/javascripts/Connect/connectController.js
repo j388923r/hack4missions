929bf2f6dd6b45416594320421d798f7c7447a3a
Nicholas Ward <kingsyphax@berkeley.edu>
2015-12-29 16:40:48 -0600

    Connect front-end JS (and HTML for testing)

--- public/javascripts/Connect/connectController.js
+++ public/javascripts/Connect/connectController.js
@@ -3,3 +3,15 @@ var myApp = angular.module('myApp');
 myApp.controller('connectController', ['$scope', function($scope) {
   $scope.greeting = 'Hola!';
 }]);
+
+$(function() {
+    $("input#tags_submit").click(function(event) {
+        event.preventDefault();
+
+        var tags = $("input#tags_input").val().split(" ");
+
+        $.get("", tags, function(data) {
+            $("div#mentor_names").html(data);
+        });
+    });
+});

8c81cfa785fb59c881b9407851eda4d476c82be5
Nicholas Ward <kingsyphax@berkeley.edu>
2015-12-29 15:58:35 -0600

    added a space

--- models/users.js
+++ models/users.js
@@ -24,6 +24,6 @@ userSchema.statics.addExpertise = function(id, expertise) {
 /* Return all documents from the Users table
  * which match all the tags in the TAGS array. */
 userSchema.statics.findByTags = function(tags_list) {
-    return this.find({ tags : { $all : tags_list}});
+    return this.find({ tags : { $all : tags_list }});
 }
 

3cd7b594bdb304753d13f8d1d26013860f4be1c0
Nicholas Ward <kingsyphax@berkeley.edu>
2015-12-30 16:48:06 -0600

    SELECT ANDREW NGUYEN YAAAY

--- public/javascripts/Connect/connectController.js
+++ public/javascripts/Connect/connectController.js
@@ -2,18 +2,6 @@ var myApp = angular.module('myApp');
 
 myApp.controller('connectController', ['$scope', function($scope) {
     $scope.greeting = 'Hola!';
-    var parameters = { tags : {} };
-      // $.post('/mentors/search', parameters, function (data) {
-      //   //Populates the dropdown with a list of mentors
-      // });
-
-    $('.mentor-search').chosen({width: '50%'});
-    $('.mentor-search').on('change', function(data) {
-        var parameters = { name: $(this).val() };
-        $.post('/mentors/searchByName', parameters, function (data) {
-            $('#mentor_names').html(data);
-        });
-    });
 
     $(function() {
         console.log("loaded");
@@ -50,7 +38,10 @@ myApp.controller('connectController', ['$scope', function($scope) {
             console.log("parameters:" + parameters);
             $.post("/mentors/search", parameters, function(data) {
                 console.log(data);
-                $("div#mentor_names").html(data);
+                $("div#mentor_names").html("");
+                for (var index in data) {
+                    $("div#mentor_names").append("<div class='mentor'>" + data[index].username + "</div>");
+                }
             });
         });
     });

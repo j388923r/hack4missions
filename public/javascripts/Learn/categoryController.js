d97ffba38267fac07e513444af4dbac7baf6f35c
gswu <gswu@gswus-MacBook-Pro.local>
2015-12-30 16:53:03 -0600

    updated category.htmls

--- public/javascripts/Learn/categoryController.js
+++ public/javascripts/Learn/categoryController.js
@@ -2,5 +2,5 @@ var myApp = angular.module('myApp');
 
 myApp.controller('categoryController', ['$scope', '$routeParams', function($scope, $routeParams) {
     $scope.category = $routeParams.tag;
-    
+    $(".chosen-select").chosen();
 }]);

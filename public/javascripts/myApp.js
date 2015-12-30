6b5affbe273c54a5da280515150e17048fb139ad
Joshua Ching <jchingdev@gmail.com>
2015-12-30 16:11:13 -0600

    userview controller

--- public/javascripts/myApp.js
+++ public/javascripts/myApp.js
@@ -40,5 +40,8 @@ myApp.config(['$routeProvider', function($routeProvider) {
   }).when('/ask', {
 	  controller : 'askController',
 	  templateUrl : '/angularviews/ask.html'	 
+  }).when('/userView', {
+	controller : 'userViewController',
+	templateUrl : '/angularviews/userView.html'
   });
 }]);
